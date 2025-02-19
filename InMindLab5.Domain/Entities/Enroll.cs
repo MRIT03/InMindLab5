506fa389d17ad05f54ad7358f95fc246421c4514
Riad Terek <Riadot03@gmail.com>
2025-02-19 16:11:45 +0200

    added the enrollement class (I forgot about it :( )

---
+++ InMindLab5.Domain/Entities/Enroll.cs
@@ -0,0 +1,8 @@
+﻿namespace InMindLab5.Domain.Entities;
+
+public class Enroll
+{
+    public required int EnrollId { get; set; }
+    public required int StudentId { get; set; }
+    public required int CourseId { get; set; }
+}
