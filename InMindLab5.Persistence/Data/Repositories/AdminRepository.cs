ebc3366304c8cb865e45d3000793f9e41e2dc6be
Riad Terek <Riadot03@gmail.com>
2025-02-21 09:38:59 +0200

    Generated the Admin, Course, Student, and Teacher repositories

---
+++ InMindLab5.Persistence/Data/Repositories/AdminRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InMindLab5.Domain.Entities;
+using InMindLab5.Persistence.Data;
+
+namespace InMindLab5.Persistence.Data.Repositories;
+
+public class AdminRepository : IRepository<Admin>
+{
+    private readonly UmcContext _dbContext;
+
+    public AdminRepository(UmcContext dbContext)
+    {
+        _dbContext = dbContext;
+        Query = _dbContext.Admins;
+    }
+
+    public IQueryable<Admin> Query { get; }
+
+    public async Task<List<Admin>> GetAllAsync()
+    {
+        return await _dbContext.Admins.ToListAsync();
+    }
+
+    public async Task AddAsync(Admin entity)
+    {
+        await _dbContext.AddAsync(entity);
+        await SaveAsync();
+    }
+
+    public async Task DeleteAsync(Admin entity)
+    {
+        _dbContext.Remove(entity);
+        await SaveAsync();
+    }
+
+    public async Task SaveAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
