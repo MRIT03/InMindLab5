d815d2de0a970e0e0279dded01653fad62c81fe2
Riad Terek <Riadot03@gmail.com>
2025-02-25 22:25:50 +0200

    added serilog logging

--- InMindLab5.API/Controllers/HomeController.cs
+++ InMindLab5.API/Controllers/HomeController.cs
@@ -2,18 +2,21 @@ using Asp.Versioning;
 using InMindLab5.API;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
 
 
 [ApiVersion(1.0)]
 [ApiController]
 [Route("api/v{apiVersion:apiVersion}/[controller]")]
-public class SampleController : ControllerBase
+public class HomeController : ControllerBase
 {
     private readonly IStringLocalizer<SharedResource> _localizer;
+    private readonly ILogger<HomeController> _logger;
 
-    public SampleController(IStringLocalizer<SharedResource> localizer)
+    public HomeController(IStringLocalizer<SharedResource> localizer, ILogger<HomeController> logger)
     {
         _localizer = localizer;
+        _logger = logger;
     }
 
     [HttpGet("test")]
@@ -23,5 +26,15 @@ public class SampleController : ControllerBase
         var message = _localizer["Hello"];
         Console.WriteLine("message: " +message);
         return Ok(new { Message = message });
+    }
+    
+    [HttpGet("logtest")]
+    public IActionResult LogTest()
+    {
+        _logger.LogInformation("This is an informational message.");
+        _logger.LogWarning("This is a warning!");
+        _logger.LogError("This is an error message!");
+        
+        return Ok("Logs have been recorded.");
     }
 }
