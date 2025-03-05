68f09b9b0afa0ba2cd53eb351e48eb2e26b0c471
Riad Terek <Riadot03@gmail.com>
2025-03-05 19:54:41 +0200

    Divided the database into several schemas and isolated each tenant in different controllers

---
+++ InMindLab5.Application/Queries/RetrieveStudentQuery.cs
@@ -0,0 +1,9 @@
+﻿using InMindLab5.Application.ViewModels;
+using MediatR;
+
+namespace InMindLab5.Application.Queries;
+
+public class RetrieveStudentQuery : IRequest<IEnumerable<StudentDto>>
+{
+    
+}
--- InMindLab5.Persistence/Data/UmcContext.cs
+++ InMindLab5.Persistence/Data/UmcContext.cs
@@ -16,13 +16,33 @@ public class UmcContext : DbContext
     public DbSet<Admin> Admins { get; set; }
     public DbSet<Enroll> Enrollments { get; set; }
     public DbSet<TeacherCourse> TeacherCourses { get; set; }
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Admin schema
+        modelBuilder.Entity<Admin>()
+            .ToTable("Admins", schema: "AdminSchema");
+        
         modelBuilder.Entity<Course>()
             .HasOne(c => c.Admin) 
             .WithMany(a => a.Courses) 
             .HasForeignKey(c => c.AdminId) 
             .OnDelete(DeleteBehavior.Restrict); 
+        
+
+        // Teacher schema
+        modelBuilder.Entity<Teacher>()
+            .ToTable("Teachers", schema: "TeacherSchema");
+        
+        modelBuilder.Entity<TeacherCourse>()
+            .ToTable("TeacherCourses", schema: "TeacherSchema");
+
+        // Student schema
+        modelBuilder.Entity<Student>()
+            .ToTable("Students", schema: "StudentSchema");
+
+        
+
+        base.OnModelCreating(modelBuilder);
     }
 }
